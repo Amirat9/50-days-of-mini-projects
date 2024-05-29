4cd478c174f189a3fb84cb52c4b2e8ce79ab7e89
ataherian@wisc.edu <amirat9@icloud.com>
2024-05-29 15:55:17 -0500

    added comments

--- day-01/script.js
+++ day-01/script.js
@@ -1,7 +1,7 @@
+// NodeList of all the panel elements
 const panels = document.querySelectorAll('.panel');
 
-console.log(panels);
-
+// Loop through all the panels and add an event listener to each panel
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
     removeActiveClasses();
@@ -9,6 +9,7 @@ panels.forEach((panel) => {
   });
 });
 
+// Function to remove the active class from all the panels
 function removeActiveClasses() {
   panels.forEach((panel) => {
     panel.classList.remove('active');
